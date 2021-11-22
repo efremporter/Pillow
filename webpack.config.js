882b2646900af0d40a04bd195129c61f48c72618
Efrem <eopiscool@gmail.com>
2021-11-22 10:09:41 -0800

    Fixing branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-var path = require('path');
-
+const path = require('path');
 module.exports = {
-  entry: './entry.jsx',
-  output: {
-    filename: './bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  }
+    context: __dirname,
+    entry: './frontend/entry.jsx', //half_time is just an example
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map'
 };
